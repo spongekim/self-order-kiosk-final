ed6dd4c7750bba5cc88be63b1cb32f751c201e6d
yeonwoo-kim <yeonwoo.kim@samsung.com>
2021-06-02 22:22:19 +0900

    test websocket echoing

--- src/WebsocketClient.js
+++ src/WebsocketClient.js
@@ -6,17 +6,26 @@ import { Store } from './Store';
 
 function WebsocketClient(props) {
   const { dispatch } = useContext(Store);
+  //const { state, dispatch } = useContext(Store);
+  //const websocket_message = state.websocket_incoming_message;
+  var send_count =0;
   const ws = useRef(null);
+  //const [number, setNumber] = useState(0);
   useEffect(() => {
     // 컴포넌트가 마운트 되고 실행됨
-    //ws.current = new WebSocket('wss://echo.websocket.org');
-    ws.current = new WebSocket('ws://localhost:21056');
+    ws.current = new WebSocket('wss://echo.websocket.org');
+    //ws.current = new WebSocket('ws://localhost:21056');
     ws.current.onopen = () => {
         console.log("ws opened");
+        setInterval( _ =>{
+          var send_data =  String(send_count++)//Math.random();
+          console.log(`message sent:${send_data}`);
+          ws.current.send(send_data )
+        }, 5000 )
     }
     ws.current.onclose = () => console.log("ws closed");
   }, []);    //<--- 두번째 인자로 빈 배열 넣어주기 - 아니면 한번만 실행 안됌
-  
+
   useEffect(() => {
     if (!ws.current) return;
     ws.current.onmessage = e => {
